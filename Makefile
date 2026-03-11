82beeddf9be850573e8859c65c92385f449e4ff6
jonahmer22 <jonah@jonahmerriam.net>
2026-03-11 16:34:39 -0400

    Completed basic Arena
    Completed a basic Arena implementation in main.c to be used for
    preliminary testing.

--- Makefile
+++ Makefile
@@ -1,5 +1,28 @@
-all:
-	gcc main.c ./src/*.c -I ./include -o ./cortex-vm
+CC      := gcc
+CFLAGS  := -std=c17 -Wall -Wextra -Wpedantic -g
+LDFLAGS :=
+
+SRC_DIR    := src
+INC_DIR    := include
+BUILD_DIR  := build
+TARGET     := cortex-vm
+
+SRCS := $(wildcard $(SRC_DIR)/*.c) main.c
+OBJS := $(patsubst %.c,$(BUILD_DIR)/%.o,$(SRCS))
+
+.PHONY: all clean run
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+	$(CC) $(LDFLAGS) $^ -o $@
+
+$(BUILD_DIR)/%.o: %.c
+	@mkdir -p $(dir $@)
+	$(CC) $(CFLAGS) -I$(INC_DIR) -c $< -o $@
+
+run: $(TARGET)
+	./$(TARGET) $(RUN_ARGS)
 
 clean:
-	rm ./cortex-vm
+	rm -rf $(BUILD_DIR) $(TARGET)
--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+// basic size of memory block for arena
+#define MEMBLOCK_SIZE 1024*1024	// 1mb by default
+
+// ===========
+// Basic Utils
+// ===========
+
+// Read a file into a buffer
 char *readFile(const char *path, size_t *outLen){
 	FILE *file = fopen(path, "rb");	// read as binary
 	if(!file){
@@ -37,12 +45,151 @@ char *readFile(const char *path, size_t *outLen){
 	return buffer;
 }
 
+// TODO: make a file reader for pure binary list of uint64_t words instead of as a char buffer like the one above
+
+// Basic Arena
+
+typedef struct MemBlock{
+	uint8_t *block;	// pointer to the base of the memory block
+
+	uint8_t *head;	// pointer to the next open byte of memory
+	
+	size_t blockSize;	// holds the total size of the block
+	size_t free;		// holds the number of free bytes left in the block
+
+
+	struct MemBlock *next;
+} MemBlock;
+
+MemBlock *memBlockInit(size_t blockSize){
+	// allocate the MemBlock struct
+	MemBlock *result = malloc(sizeof(MemBlock));
+	if(!result){
+		fprintf(stderr, "[FATAL 0x%04X]: Not enough memory to initialize MemBlock struct of size %zu bytes.\n", 0x0101, sizeof(MemBlock));
+		exit(EXIT_FAILURE);
+	}
+
+	// allocate the block of memory in the MemBlock
+	result->head = result->block = malloc(sizeof(uint8_t) * blockSize);
+	if(!result->head || !result->block){
+		fprintf(stderr, "[FATAL 0x%04X]: Not enough memory to allocate block of size %zu bytes in MemBlock.\n", 0x0102, blockSize);
+		exit(EXIT_FAILURE);
+	}
+
+	// set the amount of free bytes and amount of bytes in the block
+	result->blockSize = result->free = blockSize;
+
+	// set next to null
+	result->next = NULL;
+
+	// return result
+	return result;
+}
+
+MemBlock *memBlockDestroy(MemBlock *memBlock){
+	free(memBlock->block);
+	
+	MemBlock *result = memBlock->next;
+
+	// set all pointers to NULL
+	memBlock->block = memBlock->head = NULL;
+	memBlock->next = NULL;
+	// set all values to 0
+	memBlock->blockSize = memBlock->free = 0;
+
+	free(memBlock);
+
+	return result;
+}
+
+typedef struct Arena{
+	MemBlock *head;
+	MemBlock *currBlock;
+
+	size_t numBlocks;
+} Arena;
+
+static Arena arena;
+
+void arenaInit(void){
+	arena.head = arena.currBlock = NULL;
+	arena.numBlocks = 0;
+}
+
+void arenaDestroy(void){
+	// if arena is NULL then reset the global arena
+	if(arena.head){
+		// if there is a head but not a currBlock then something is deeply wrong, just leak the memory and get out
+		if(!arena.currBlock){
+			fprintf(stderr, "[FATAL 0x%04X]: Error in Arena configuration, MemBlock head (%p) is present, but currBlock (%p) is not\n.", 0x0103, (void *)arena.head, (void *)arena.currBlock);
+			exit(EXIT_FAILURE);
+		}
+
+		// free all MemBlocks
+		for(MemBlock *temp = memBlockDestroy(arena.head); temp != NULL; temp = memBlockDestroy(temp));
+	}
+
+	// set everything to 0 and NULL then exit
+	arena.head = arena.currBlock = NULL;
+	arena.numBlocks = 0;
+}
+
+void *arenaAlloc(size_t size){
+	// if the size of the allocation is lower than the size of the normal block
+	if(size < MEMBLOCK_SIZE)
+		size = MEMBLOCK_SIZE;
+	else{
+		// in the case that it isn't we have to make a new block of the propper size and mark it as entirely full
+		arena.currBlock->next = memBlockInit(size);
+		arena.currBlock = arena.currBlock->next;
+
+		arena.currBlock->free = 0;
+		arena.currBlock->head += arena.currBlock->blockSize;
+
+		return arena.currBlock->block;
+	}
+
+	// if there is no memblocks yet
+	if(!arena.head)
+		arena.head = arena.currBlock = memBlockInit(size);
+
+	if(arena.currBlock->free < size){
+		arena.currBlock->next = memBlockInit(MEMBLOCK_SIZE);
+		arena.currBlock = arena.currBlock->next;
+
+		arena.currBlock->free -= size;
+		arena.currBlock->head += size;
+
+		return arena.currBlock->block;
+	}
+	
+	void *base = arena.currBlock->head;
+	arena.currBlock->head += size;
+	arena.currBlock->free -= size;
+
+	return base;
+}
+
 int main(int argc, char **argv){
 	// load a file in
 	size_t size = 0;
 	char *buff = readFile(argv[1], &size);
 
-	printf("%s\n%zu\n", buff, size);
+	arenaInit();
+
+	int *t = arenaAlloc(sizeof(int) * 256);
+
+	for(int i = 0; i < 256; i++){
+		t[i] = 256 - i;
+	}
+	for(int i = 256; i >= 0; i--){
+		printf("%d ", t[i]);
+	}
+	printf("\n");
+
+	arenaDestroy();
+
+	printf("%s\n%zu\n%p\n", buff, size, (void *)&arena);
 
 	return 0;
 }
