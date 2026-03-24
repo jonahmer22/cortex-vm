e25a173cbcce8a33aa216c277ec17eebee2de367
jonahmer22 <jonah@jonahmerriam.net>
2026-03-24 13:53:31 -0400

    fixed missing include

--- include/core.h
+++ include/core.h
@@ -2,6 +2,7 @@
 #define CORE_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 void run(uint64_t *regs, uint64_t *codeBase,/* uint64_t *heapBase,*/ uint64_t *stackBase, uint64_t fileLength);
 bool step(uint64_t *regs, uint64_t *codeBase,/* uint64_t *heapBase,*/ uint64_t *stackBase, uint64_t fileLength);
