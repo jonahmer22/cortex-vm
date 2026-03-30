e9bc5b7b60b5ae6d6e485b8ecea69464e6a3117b
jonahmer22 <jonah@jonahmerriam.net>
2026-03-29 20:17:51 -0400

    Completed implementation of M extensions
    Completed implementation of M extensions; added 2 test files test7.s
    which is a simple test of muli and test8.s which tests all new added
    opcodes (which appear to be functional)

--- src/core.c
+++ src/core.c
@@ -349,7 +349,7 @@ static bool handleSyscall(uint64_t *regs, uint64_t *codeBase, uint64_t *stackBas
 			#ifdef _WIN32
 			Sleep((DWORD)ms);
 			#else
-			usleep((uint64_t)(ms * 1000));
+			sleep((uint64_t)(ms * 1000));
 			#endif
 			break;
 		}
